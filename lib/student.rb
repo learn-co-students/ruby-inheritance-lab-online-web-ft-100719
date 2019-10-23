bae69e5bb1f1e4cdc4a41bdf08e68ca033b254cf
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-10-23 13:10:28 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,26 @@
- require_relative "./user.rb"
+
 
 class Student < User
+attr_accessor :firstname, :lastname
+
+def initialize
+  @knowledge = []
+end
+
+def first_name
+  @firstname
+end
+
+def last_name
+  @lastname
+end
+
+def learn(stuff)
+  @knowledge << stuff
+end
+
+def knowledge
+  @knowledge
+end
 
 end
--- lib/user.rb
+++ lib/user.rb
@@ -3,8 +3,8 @@ class User
 attr_accessor :firstname, :lastname
 
 def initialize
-  @firstname = first
-  @lastname = last
+  @firstname = firstname
+  @lastname = lastname
 end
 
 end
